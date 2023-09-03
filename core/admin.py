f565fea1cdd499a3bbf8fe98664d441e1c2c6d94
APICHART KANSEENUAL <apichart.dev@gmail.com>
2023-09-03 22:32:41 +0700

    S5

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import rfinv_loc,rfinv_inv,rfinv_check
+
+"""
+admin.site.register(rfinv_loc)
+admin.site.register(rfinv_inv)
+admin.site.register(rfinv_check)
+"""
+@admin.register(rfinv_loc)
+class rfinv_locAdmin(admin.ModelAdmin):
+    list_display = ('Loc_ID','Loc_Name')
+    #list_filter = ('accType')
+    #search_fields = ('accCode','accName')
+    #prepopulated_fields = {'accName':('accCode')}
+    #raw_id_fields=('accType')
+    #date_hierarchy = ('CreateDate')
+    #ordering = ('accCode')
+
+@admin.register(rfinv_inv)
+class rfinv_invAdmin(admin.ModelAdmin):
+    list_display = ('Inv_ID','Loc_ID','Inv_Name','Inv_Last_Check_Time','Inv_Last_Loc')
+    
+@admin.register(rfinv_check)    
+class rfinv_checkAdmin(admin.ModelAdmin):
+    list_display = ('Chk_ID','Inv_ID','Loc_ID','Chk_Time')        
+    
+"""
+@admin.register(tbAccount)
+class tbAccountAdmin(admin.ModelAdmin):
+    list_display = ('accCode','accName','accType')
+    #list_filter = ('accType')
+    #search_fields = ('accCode','accName')
+    #prepopulated_fields = {'accName':('accCode')}
+    #raw_id_fields=('accType')
+    #date_hierarchy = ('CreateDate')
+    #ordering = ('accCode')
+
+@admin.register(tbProject)
+class tbProjectAdmin(admin.ModelAdmin):
+    list_display = ('prjID','prjType','prjG1','prjG2','prjG3','prjDesc','prjYear','prjBudget')
+
+@admin.register(tbAccMatch)
+class tbAccMatchAdmin(admin.ModelAdmin):
+    #list_display = ('amID','accCode','prjID','amDesc')
+    list_display = ('prjID',)
+
+@admin.register(tbProject_Transaction)
+class tbProject_TransactionAdmin(admin.ModelAdmin):
+    list_display = ('trID','amID','trDesc','tnsDate','tnsAmount','tnsType')
+"""    
+    
--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class rfinv_loc(models.Model):
+    Loc_ID = models.CharField(max_length=255,primary_key=True, editable=True, unique=True)
+    Loc_Name = models.CharField(max_length=255, blank = True, null = True)
+    
+    class Meta:
+        ordering = ('Loc_ID',)
+        
+    def __str__(self):
+        return str(self.Loc_ID)  + " : " + str(self.Loc_Name)
+    
+class rfinv_inv(models.Model):
+    Inv_ID = models.CharField(max_length=255,primary_key=True, editable=True, unique=True)
+    Loc_ID = models.ForeignKey(rfinv_loc, on_delete=models.CASCADE, related_name='InvLocationID') 
+    Inv_Name = models.CharField(max_length=255, blank = True, null = True)
+    Inv_Last_Check_Time = models.DateTimeField(auto_now_add=True)
+    Inv_Last_Loc = models.CharField(max_length=255, blank = True, null = True)
+    
+    class Meta:
+        ordering = ('Inv_ID',)
+        
+    def __str__(self):
+        return str(self.Inv_ID)  + " : " + str(self.Inv_Name)
+    
+class rfinv_check(models.Model):
+    Chk_ID = models.AutoField(primary_key=True, editable=True, auto_created = True, unique=True)
+    Inv_ID = models.ForeignKey(rfinv_inv, on_delete=models.CASCADE, related_name='ChkInventoryID') 
+    Loc_ID = models.ForeignKey(rfinv_loc, on_delete=models.CASCADE, related_name='ChkLocationID') 
+    Chk_Time = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('Chk_ID',)
+        
+    def __str__(self):
+        return str(self.Chk_ID)  
+            
+    
+    
