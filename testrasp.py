731472e98e0707a0649c838eda1f697b162dbd67
kipkea <apichart.dev@mail.com>
2026-05-08 14:24:47 +0700

    B112

--- testrasp.py
+++ testrasp.py
@@ -6,18 +6,22 @@ import requests
 #API key for keaCom is: swQuMMgt.vh1nQPMnHmNtClcTQM5DOcpjhHv4X0RA
  
 #dev
-url="http://localhost:8000/api/basic/"
-key = "vaGkQQur.OkotzgLTEDFuXwzZrUA1oMUH7iKWDugW"
+#url="http://localhost:8000/api/RFIDTags/"
+#key = "vaGkQQur.OkotzgLTEDFuXwzZrUA1oMUH7iKWDugW"
 #API key for keaCom is: swQuMMgt.vh1nQPMnHmNtClcTQM5DOcpjhHv4X0RA
-key = "swQuMMgt.vh1nQPMnHmNtClcTQM5DOcpjhHv4X0RA"
+#key = "swQuMMgt.vh1nQPMnHmNtClcTQM5DOcpjhHv4X0RA"
 #API Key for PI3
-key = "TM4fc8ew.yIeDMRVam9qvQvyGr68n3EpXirAdwv5h"
+#key = "TM4fc8ew.yIeDMRVam9qvQvyGr68n3EpXirAdwv5h"
 #BeeComp
 #key = "lR95LKJ5.bY6X3kf2sIgBIklwLnZ18l7RbwTNllBF"
+#VegaCom
+#key = "BJ52fz5N.4536zhdh3XhRKbM8QJ27G9qmO8dkjuhE"
 
 #aws
 #url="http://ec2-52-20-131-209.compute-1.amazonaws.com/api/basic/"
+url="http://ec2-52-20-131-209.compute-1.amazonaws.com/api/RFIDTags/"
 #key = "fS6yn9J2.V0nZcnDAG5Jf6uZHafXQa3R1a2aqSJbe"
+key = "CFiTkTbk.JWUqGSLUUqrTbhOSOteivmEachoDGeMy"
 
 cmd="curl -s -k -H Authorization: Api-Key " + key + " " + url
 print (url)
